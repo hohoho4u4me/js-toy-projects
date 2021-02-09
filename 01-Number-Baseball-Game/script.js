2c9aff1ac48c3e4fd529ffc8c7c2b29ea5c4125a
hohoho4u4me <63030091+hohoho4u4me@users.noreply.github.com>
2021-02-09 15:10:00 +0900

    Project 1 - Added function to summit / win

--- 01-Number-Baseball-Game/script.js
+++ 01-Number-Baseball-Game/script.js
@@ -1,5 +1,6 @@
 "use-strict";
 
+let answerValue = [];
 let ballValue = [];
 let ballIndex = 0;
 let round = 0;
@@ -26,24 +27,44 @@ const deleteNumber = function () {
 const submitNumber = function () {
   if (round < 9) {
     if (ballIndex == 3) {
+      let strike = 0;
+      let ball = 0;
       for (let i = 0; i < 3; i++) {
         previousRecordRow[round].childNodes[3].childNodes[2 * i + 1].innerText =
           ballValue[i];
         inputRowInput[i].innerText = "";
+        if (answerValue[i] == ballValue[i]) strike++;
+        else if (answerValue.includes(ballValue[i])) ball++;
+        ballValue[i] = "";
+      }
+      previousRecordRow[round].childNodes[5].childNodes[1].innerText = strike;
+      previousRecordRow[round].childNodes[5].childNodes[5].innerText = ball;
+      if (strike == 3) {
+        alert("win!!");
+        round = 9;
       }
       ballIndex = 0;
       round++;
       if (round == 9) {
         alert("game over");
-      } else {
+      } else if (round < 9) {
         previousRecordRow[round].classList.remove("hidden");
       }
     }
   }
 };
+const setAnswerValue = function () {
+  for (let i = 0; i < 3; i++)
+    answerValue[i] = Math.trunc(Math.random() * (10 - i));
+  if (answerValue[0] <= answerValue[1]) answerValue[1]++;
+  if (Math.min(answerValue[0], answerValue[1]) <= answerValue[2])
+    answerValue[2]++;
+  if (Math.max(answerValue[0], answerValue[1]) <= answerValue[2])
+    answerValue[2]++;
+};
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++)
   numberBoardBtn[i].addEventListener("click", enterNumber);
-}
 inputRowBtn[0].addEventListener("click", deleteNumber);
 inputRowBtn[1].addEventListener("click", submitNumber);
+setAnswerValue();
