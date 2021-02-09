64398ac1ef2bd4ae60440b04354a7bd726911949
hohoho4u4me <63030091+hohoho4u4me@users.noreply.github.com>
2021-02-09 14:19:33 +0900

    Project 1 - Added submit function

--- 01-Number-Baseball-Game/script.js
+++ 01-Number-Baseball-Game/script.js
@@ -2,14 +2,17 @@
 
 let ballValue = [];
 let ballIndex = 0;
-let Round = 0;
+let round = 0;
 
 const numberBoardBtn = document.querySelectorAll(".number-board__btn");
+const inputRowInput = document.querySelectorAll(".input-row__input");
 const inputRowBtn = document.querySelectorAll(".input-row__btn");
+const previousRecordRow = document.querySelectorAll(".previous-record__row");
 
 const enterNumber = function () {
   if (0 <= ballIndex && ballIndex <= 2) {
     ballValue[ballIndex] = Number(this.innerText);
+    inputRowInput[ballIndex].innerText = ballValue[ballIndex];
     ballIndex++;
   }
 };
@@ -17,11 +20,28 @@ const deleteNumber = function () {
   if (ballIndex > 0) {
     ballValue.pop();
     ballIndex--;
+    inputRowInput[ballIndex].innerText = "";
+  }
+};
+const submitNumber = function () {
+  if (round < 9) {
+    if (ballIndex == 3) {
+      for (let i = 0; i < 3; i++) {
+        previousRecordRow[round].childNodes[3].childNodes[2 * i + 1].innerText =
+          ballValue[i];
+        inputRowInput[i].innerText = "";
+      }
+      ballIndex = 0;
+      round++;
+      if (round == 9) {
+        alert("game over");
+      }
+    }
   }
 };
 
 for (let i = 0; i < 10; i++) {
   numberBoardBtn[i].addEventListener("click", enterNumber);
 }
-console.log(inputRowBtn);
 inputRowBtn[0].addEventListener("click", deleteNumber);
+inputRowBtn[1].addEventListener("click", submitNumber);
